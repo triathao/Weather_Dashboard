1cb3c6a731615378471ab006c6ce70bf154b4038
Tria Thao <triathao@gmail.com>
2020-11-07 00:20:02 -0800

    js links correction

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-var apikey = "fa62741cbe1bbb117e7949e530a3f6d3"
+let apikey = "fa62741cbe1bbb117e7949e530a3f6d3";
+
 console.log(apikey)
 
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + "sacramento" + "&appid=" + apikey;
+let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + "sacramento" + "&appid=" + apikey;
 
-console.log(queryURL)
+console.log(queryURL);
 
 $.ajax({
     url: queryURL,
     method: "GET"
-}).then (function(response){
+}).then(function(response){
+console.log(response)
 
 })
+
+
+
+
